b6742519b5aab6ff3b4eb9356b1b9fd9214fefa5
James Henry <jameshenry@JAMESS-MAC-MINI.station>
2026-09-02 20:09:48 +0100

    navigation work

--- app.py
+++ app.py
@@ -6,3 +6,24 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/trips")
+def trips():
+    return render_template("trips.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+# Functions
+
+# 
+# Display trips()
+# create a trip()
+# itinerary ()
+# budget()
+# calculate_total()
+# budget_percentage()
+# add_memory()
+# get_next_trip()
+
+# get and post for create trips, add memories(images), budget, itinerary
