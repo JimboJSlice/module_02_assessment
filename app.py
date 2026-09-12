7ab306a06f23c1f58cd6e04afc7ecc67e4f205bf
James Henry <jameshenry@JAMESS-MAC-MINI.station>
2026-09-12 16:51:50 +0100

    Added default URL + comments

--- app.py
+++ app.py
@@ -10,17 +10,21 @@ app.secret_key = os.environ['SECRET_KEY']
 @app.route("/")
 def home():
 
+    # Create separate lists so trips can be diplayed according to their status
     upcoming_trips = []
     planning_trips = []
     completed_trips = []
 
+    # These variable are used to calculate the travel statistics
     total_budget = 0
     total_spent = 0
     total_activities = 0
     total_memories = 0
 
+    # Loop through every trip and collect information
     for trip in trips:
 
+        # add to the appropriate lists
         if trip["status"] == "Upcoming":
             upcoming_trips.append(trip)
 
@@ -47,7 +51,7 @@ def home():
     remaining_budget = total_budget - total_spent
 
     # Calculate percentage of budget spent
-    if total_budget > 0:
+    if total_budget > 0: # To stop dividing by 0 error
         budget_percentage = round(
             total_spent / total_budget * 100
         )
@@ -79,7 +83,7 @@ def home():
 
 @app.route("/all_trips")
 def all_trips():
-
+    # Creates the lists for this page
     upcoming_trips = []
     planning_trips = []
     completed_trips = []
@@ -107,13 +111,14 @@ def all_trips():
 @app.route("/trip/<int:trip_id>")
 def trip_details(trip_id):
 
-    selected_trip = None
+    selected_trip = None # Start with no ID and allows application to handle invalid IDs
 
+    # Loop to search for ID provided in the URL when the users selects
     for trip in trips:
 
         if trip["id"] == trip_id:
             selected_trip = trip
-            break
+            break # Stop once matched
 
     if selected_trip:
 
@@ -122,8 +127,10 @@ def trip_details(trip_id):
             for expense in selected_trip["expenses"]
         )
 
+        # Remaining budget logic
         remaining = selected_trip["budget"] - total_spent
 
+        # Calculate the percentage
         budget_percentage = (
             round(
                 total_spent
@@ -131,11 +138,11 @@ def trip_details(trip_id):
                 * 100
             )
             if selected_trip["budget"] > 0
-            else 0
+            else 0 # Divide by 0 prevention
         )
 
     else:
-
+        # Default values for no entry
         total_spent = 0
         remaining = 0
         budget_percentage = 0
@@ -154,23 +161,28 @@ def create_trip():
 
     if request.method == "POST":
 
+        # Retrieve these details from the form
         destination = request.form["destination"]
         start_date = request.form["start_date"]
         end_date = request.form["end_date"]
         budget = request.form["budget"]
         description = request.form["description"]
         trip_type = request.form["trip_type"]
+
+        # Get for optional URl
         image_url = request.form.get("image_url")
 
-        # Get activity fields
+        # Get from list for multiple entries
         activity_days = request.form.getlist("activity_day")
         activity_times = request.form.getlist("activity_time")
         activity_names = request.form.getlist("activity_name")
 
         activities = []
 
+        # Loop through the submitted activies
         for position in range(len(activity_names)):
 
+            # Only create activity if all fields contain data
             if (
                 activity_names[position]
                 and activity_days[position]
@@ -195,6 +207,7 @@ def create_trip():
 
         for position in range(len(expense_descriptions)):
 
+            # Only add if both fields have been filled
             if (
                 expense_descriptions[position]
                 and expense_amounts[position]
@@ -206,7 +219,7 @@ def create_trip():
                     "amount": float(expense_amounts[position])
                 })
 
-        # Create the new trip
+        # Create the new trip dictionary using the fields from the form
         new_trip = {
             "id": len(trips) + 1,
             "destination": destination,
@@ -215,13 +228,14 @@ def create_trip():
             "budget": float(budget),
             "description": description,
             "trip_type": trip_type,
-            "status": "Planning",
-            "image": image_url or "default-trip.jpg",
+            "status": "Planning", # New trips start in Planning. Future addition to edit this
+            "image": image_url or "https://picsum.photos/id/27/1000/1400", # Use a default URL if blank
             "activities": activities,
             "expenses": expenses,
-            "memories": []
+            "memories": [] # Blank as nothing to add
         }
 
+        # Add to the main trips list
         trips.append(new_trip)
 
         return redirect(url_for("all_trips"))
